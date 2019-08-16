84b561fd100e0622c7b465f1e1b32acc59de535f
Paul Brown <paulbrown1982@yahoo.co.uk>
2019-08-16 12:07:58 +0100

    Created an Export of the pixel tracker's CF Logs S3 bucket ready to be used by the archiver.

--- email-mvt-pixel/lib/EmailMVTPixel.ts
+++ email-mvt-pixel/lib/EmailMVTPixel.ts
@@ -1,7 +1,7 @@
 import cdk = require('@aws-cdk/core');
-import {Construct, Duration} from '@aws-cdk/core';
+import {Construct} from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
 import {Helper} from '../../shared-ts/helpers';
-
 
 export interface EmailPixelProps {
   certificateArn: cdk.CfnParameter;
@@ -12,19 +12,21 @@ export interface EmailPixelProps {
 
 export class EmailMVTPixel extends Construct {
 
-
   constructor(parent: Construct, name: string, props: EmailPixelProps) {
     super(parent, name);
 
     const pixelDomain = `email-${props.stageSubdomain.valueAsString}.${props.tld.valueAsString}`;
     const originAccessIdentity = Helper.createNewOriginAccessIdentity(this, pixelDomain);
     const bucketForPixel = Helper.createS3Bucket(this,'Source', pixelDomain, ['s3:GetObject'], [], false, originAccessIdentity);
-    const bucketForCFLogs = Helper.createS3Bucket(this,'Logs', pixelDomain, [''], [Helper.twoWeekLifecycleRule],true);
+    const bucketForCFLogs: s3.Bucket = Helper.createS3Bucket(this,'Logs', pixelDomain, [''], [Helper.twoWeekLifecycleRule],true);
 
     const hostedZone = Helper.createNewHostedZone(this, props.hostedZoneId.valueAsString, props.tld.valueAsString);
     Helper.createCFDistribution(this, bucketForPixel, bucketForCFLogs, hostedZone, pixelDomain, props.certificateArn.valueAsString, originAccessIdentity);
 
-    // new cdk.CfnOutput(this, 'Bucket', { value: siteBucket.bucketName });
+    new cdk.CfnOutput(this, 'CFLogs_S3Bucket_Output', {
+      value: bucketForCFLogs.bucketName,
+      exportName: 'EmailMVTPixel-Logs-S3Bucket'
+    });
     // new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
   }
 }
