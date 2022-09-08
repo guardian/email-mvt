8b43548aeeb079358afe54691679e1ff007561f1
Paul Brown <paulbrown1982@yahoo.co.uk>
2022-09-08 17:22:45 +0100

    Updated some library versions, fixed lint errors in archive lambda's TS file. Improved local documentation

--- email-mvt-archive/README.md
+++ email-mvt-archive/README.md
@@ -4,9 +4,9 @@ This is repo containing a Lambda function which executes daily and copies the lo
 
 ## Instructions to build and run:
  - Setup steps (in this folder):
-   1. `brew install node`
-   2. `brew install npm`
+   1. `brew install nvm`
+   2. `nvm install`
    3. `npm install`
    4. `npm run lint`
-   6. `npm run build`
+   5. `npm run build`
    
--- email-mvt-archive/src/email-mvt-pixel-log-archiver-lambda.ts
+++ email-mvt-archive/src/email-mvt-pixel-log-archiver-lambda.ts
@@ -1,41 +1,45 @@
-import {S3} from "aws-sdk";
-import {ObjectList} from "aws-sdk/clients/s3";
+import type {S3} from "aws-sdk";
+import aws from "aws-sdk";
+import type {ObjectList} from "aws-sdk/clients/s3";
 
-const aws = require('aws-sdk');
 const s3 = new aws.S3();
 const filenameDateRegex = /(\d\d\d\d-\d\d-\d\d)-\d\d/;
 
-async function listAllObjects(s3Objects: any[], Bucket: string, ContinuationToken?: string){
+async function listAllObjects(s3Objects: S3.ObjectList, Bucket: string, ContinuationToken?: string){
   const { Contents, IsTruncated, NextContinuationToken } = await s3.listObjectsV2({ Bucket, ContinuationToken }).promise();
-  s3Objects.push(...Contents);
+  if (Contents) {
+    s3Objects.push(...Contents);
+  }
   if (IsTruncated) {
     await listAllObjects(s3Objects, Bucket, NextContinuationToken); // RECURSIVE CALL
   }
 }
 
 interface TransferableFile {
-  sourceFileName: string,
-  destinationFolder?: string,
+  sourceFileName: string;
+  destinationFolder: string;
 }
 
 function getTransferableFiles(allS3Objects: ObjectList) {
   return allS3Objects
-    .filter(s3object => filenameDateRegex.test(`${s3object.Key}`))
+    .filter(s3object => s3object.Key && filenameDateRegex.test(s3object.Key))
     .map(s3object => {
-      const s3objectValue = `${s3object.Key}`;
-      const matches = s3objectValue.match(filenameDateRegex);
-      if (matches && matches.length > 0) {
-        const dateFromFilename = matches[1];
-        const tuple: TransferableFile = { sourceFileName: s3objectValue, destinationFolder: dateFromFilename };
-        return tuple;
+      if (s3object.Key) {
+        const s3objectValue = `${s3object.Key}`;
+        const matches = s3objectValue.match(filenameDateRegex);
+        if (matches && matches.length > 0 && matches[1]) {
+          const dateFromFilename = matches[1];
+          const tuple: TransferableFile = {sourceFileName: s3objectValue, destinationFolder: dateFromFilename};
+          return tuple;
+        }
       }
     }).filter((item): item is TransferableFile => !!item);
 }
 
-function processTransfer(transferableFiles: TransferableFile[]): Promise<String | S3.Types.CopyObjectOutput>[] {
-  const sourceS3Bucket = process.env.source_s3_bucket;
-  const destinationS3Bucket = process.env.destination_s3_bucket;
-
+function processTransfer(
+    transferableFiles: TransferableFile[],
+    sourceS3Bucket: string,
+    destinationS3Bucket: string): Array<Promise<string | S3.Types.CopyObjectOutput>> {
   return transferableFiles.map(fileToTransfer => {
     return s3.headObject({
       Bucket: `${destinationS3Bucket}/dt=${fileToTransfer.destinationFolder}`,
@@ -51,7 +55,7 @@ function processTransfer(transferableFiles: TransferableFile[]): Promise<String 
   });
 }
 
-exports.handler = async () => {
+export async function handler() {
 
   const sourceS3Bucket = process.env.source_s3_bucket;
   const destinationS3Bucket = process.env.destination_s3_bucket;
@@ -62,14 +66,14 @@ exports.handler = async () => {
 
   await listAllObjects(allS3Objects, sourceS3Bucket);
 
-  const promises = processTransfer(getTransferableFiles(allS3Objects));
+  const promises = processTransfer(getTransferableFiles(allS3Objects), sourceS3Bucket, destinationS3Bucket);
   const results = await Promise.all(promises);
 
+  const failedResults = results.filter(value => value === false);
   const skippedResults = results.filter(value => value === 'skipped')
-  const failedResults = results.filter(value => !value);
-  const copiedResults = results.filter(value => value && value !== 'skipped');
+  const copiedResults = results.filter(value => value !== false && value !== 'skipped');
   const log = `Files: ${copiedResults.length} copied. ${skippedResults.length} skipped. ${failedResults.length} failed.`
 
   console.log(log);
   return log;
-};
+}
